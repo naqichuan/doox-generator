fb39c5d6d027d12440921593ff6bd070395301dd
Naqi Chuan <naqichuan@163.com>
2020-06-01 11:17:28 +0800

    M

---
+++ generator-web/src/main/java/org/nqcx/generator/DooxGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.nqcx.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooxGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DooxGeneratorApplication.class, args);
+    }
+
+}
